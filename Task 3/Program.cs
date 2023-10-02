aa4425a49c399d0528832fb8526c45de69fcb8c1
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2023-10-02 20:50:30 +0300

    Add project files.

---
+++ Task 3/Program.cs
@@ -0,0 +1,20 @@
+﻿class Test
+{
+    public static void Main()
+    {
+        string path = @"D:\Programming guysss";
+        DirectoryInfo Directory = new DirectoryInfo(path);
+        Directory.Create();
+
+
+        var initial = $@"{path}\Initial.txt";
+
+        var final = $@"{path}\Final.txt";
+
+        string hello = "hello guys";
+
+        File.WriteAllText(initial, hello);
+        File.Copy(initial, final);
+        
+    }
+}
